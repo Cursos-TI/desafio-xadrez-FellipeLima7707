71a0cc49a1b44a64f8bd44c7ec21095f1c4291cd
Fellipe Lima <fellipelima7707@gmail.com>
2025-03-27 17:04:33 +0000

    concluido

--- xadrez.c
+++ xadrez.c
@@ -4,30 +4,60 @@ int main() {
 
 int torre =  0;
 int bispo = 1;
-int rainha = 0;
+int rainha = 1;
+int cavalo;
 
-printf("Movimentando a Torre\n");
+
+    printf("\n");
+    printf("Movimentando a Torre\n");
+    printf("\n");
 
 for (int i = 0; i < 5; i++) {
     torre++;
-    printf("Direita: %d\n", torre);
+    printf("Direita \n", torre);
 }
 
+    printf("\n");
     printf("Movimentando o Bispo\n");
+    printf("\n");
 
 do
 {
-    printf("Cima, Direita : %d\n", bispo);
+    printf("Cima, Direita \n", bispo);
     bispo++;
 } while (bispo <= 5);
 
+
+    printf("\n");
     printf("Movimentando a Rainha\n");
+    printf("\n");
+
 
 while (rainha <= 8)
     {
-    printf("Esquerda : %d\n", rainha);
+    printf("Esquerda \n", rainha);
         rainha++;
     }
+
+
+    printf("\n");
+    printf("Movimentando o Cavalo\n");
+    printf("\n");
+
+
+    for(cavalo = 1; cavalo <= 2; cavalo++)
+    {
+
+        for (cavalo = 1; cavalo <= 2; cavalo++)
+        {
+            printf("Baixo \n", cavalo);
+           
+            
+        }
+        printf("Esquerda \n");
+        printf("\n");
+    }
+
 
     return 0;
 
